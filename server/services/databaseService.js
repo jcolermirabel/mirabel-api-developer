a9265219e5a39c78b4af47ed979132b5e6c3a4e9
Jestin <jestin@jestincoler.com>
2024-12-01 16:26:05 -0500

    additional troubleshooting.

--- server/services/databaseService.js
+++ server/services/databaseService.js
@@ -1,32 +1,22 @@
 const sql = require('mssql');
 
 const getConnectionConfig = (config) => {
-  const port = parseInt(config.port);
+  const connectionString = `mssql://${config.username}:${config.password}@${config.host}:${config.port}/${config.database}`;
+
+  console.log('Connection string (redacted):', 
+    connectionString.replace(config.password, '[REDACTED]')
+  );
 
   return {
-    server: config.host,
-    database: config.database,
-    user: config.username,
-    password: config.password,
+    connectionString,
     options: {
       trustServerCertificate: true,
       encrypt: false,
       enableArithAbort: true,
-      port: port,
       connectTimeout: 30000,
       requestTimeout: 30000,
-      instanceName: '',
-      useUTC: true,
-      packetSize: 32768,
       tdsVersion: '7_4'
-    },
-    pool: {
-      min: 0,
-      max: 1,
-      idleTimeoutMillis: 30000
-    },
-    stream: false,
-    parseJSON: true
+    }
   };
 };
 
@@ -35,11 +25,6 @@ const testConnection = async (config) => {
   try {
     console.log('\n=== Starting SQL Connection Test ===');
     const connectionConfig = getConnectionConfig(config);
-    
-    console.log('Connecting with config:', {
-      ...connectionConfig,
-      password: '[REDACTED]'
-    });
     
     pool = await sql.connect(connectionConfig);
     const result = await pool.request().query('SELECT 1 as test');
